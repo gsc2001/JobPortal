3581022027f5b1f16cd96b29125b07f2a8031f9c
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2021-01-20 21:21:37 +0530

    [Add] Side bar complete

--- frontend/src/components/SideDrawer/index.tsx
+++ frontend/src/components/SideDrawer/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
+import PersonIcon from '@material-ui/icons/Person';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PeopleIcon from '@material-ui/icons/People';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import ListItemText from '@material-ui/core/ListItemText';
 import Toolbar from '@material-ui/core/Toolbar';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+
+import { useTypedSelector } from '../../utils/hooks';
 
 const drawerWidth = 240;
 
@@ -23,12 +31,24 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         drawerContainer: {
             overflow: 'auto'
+        },
+        avatar: {
+            cursor: 'pointer',
+            width: 64,
+            height: 64
         }
     })
 );
 
 const SideDrawer: React.FC = () => {
     const classes = useStyles();
+    const user = useTypedSelector(state => state.auth.user);
+
+    if (!user) {
+        return <div>Please Log in </div>;
+    }
+
+    const isApplicant = user.role === 'applicant';
 
     return (
         <Drawer
@@ -40,26 +60,47 @@ const SideDrawer: React.FC = () => {
         >
             <Toolbar />
             <div className={classes.drawerContainer}>
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
+                <Box alignItems="center" display="flex" flexDirection="column" p={2}>
+                    <Avatar
+                        className={classes.avatar}
+                        component={RouterLink}
+                        src={user.avatarImage}
+                        to="/app/profile"
+                    />
+                    <Typography color="textPrimary" variant="h5">
+                        {user.name}
+                    </Typography>
+                    <Typography variant="caption">{user.email}</Typography>
+                </Box>
                 <Divider />
                 <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
+                    <ListItem button component={RouterLink} to="/app/dashboard">
+                        <ListItemIcon>
+                            <DashboardIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Dashboard" />
+                    </ListItem>
+                    <ListItem button component={RouterLink} to="/app/profile">
+                        <ListItemIcon>
+                            <PersonIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Profile" />
+                    </ListItem>
+                    {isApplicant ? (
+                        <ListItem button component={RouterLink} to="/app/applications">
                             <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                <FormatListBulletedIcon />
                             </ListItemIcon>
-                            <ListItemText primary={text} />
+                            <ListItemText primary="My Applications" />
                         </ListItem>
-                    ))}
+                    ) : (
+                        <ListItem button component={RouterLink} to="/app/employees">
+                            <ListItemIcon>
+                                <PeopleIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="My Employees" />
+                        </ListItem>
+                    )}
                 </List>
             </div>
         </Drawer>
--- frontend/src/components/TopBar/index.tsx
+++ frontend/src/components/TopBar/index.tsx
@@ -3,24 +3,46 @@ import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { useTypedSelector } from '../../utils/hooks';
+import { useDispatch } from 'react-redux';
+import { authReset } from '../../store/auth';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         appBar: {
-            zIndex: theme.zIndex.drawer + 1
+            zIndex: theme.zIndex.drawer + 1,
+            flexGrow: 1
+        },
+        title: {
+            flexGrow: 1
         }
     })
 );
 
 export default function TopBar() {
     const classes = useStyles();
-
+    const isLoggedIn = useTypedSelector(state => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
     return (
         <AppBar position="fixed" className={classes.appBar}>
             <Toolbar>
-                <Typography variant="h6" noWrap>
+                <Typography variant="h6" noWrap className={classes.title}>
                     Job Portal
                 </Typography>
+                {isLoggedIn && (
+                    <Tooltip title="Logout" arrow>
+                        <IconButton
+                            edge="end"
+                            color="inherit"
+                            onClick={() => dispatch(authReset())}
+                        >
+                            <ExitToAppIcon />
+                        </IconButton>
+                    </Tooltip>
+                )}
             </Toolbar>
         </AppBar>
     );
